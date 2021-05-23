2ad2d0220770f4759d312a9a7d29035d97c517b4
Vikash Dubey <v.dubey2311@gmail.com>
2021-05-23 08:29:42 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ViewCustomers from './components/viewCustomers'
+import SingleCustomers from './components/singleCustomer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <ViewCustomers />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ViewCustomers} />
+          <Route exact path="/single-customer" component={SingleCustomers} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
